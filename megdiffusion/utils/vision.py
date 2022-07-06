02150734b95cfbdcad525df4b584d4d372847cfd
MegChai <acdoge.cao@gmail.com>
2022-07-06 18:50:00 +0800

    🐛 fix: convert make_gird input data correctly

--- megdiffusion/utils/vision.py
+++ megdiffusion/utils/vision.py
@@ -8,13 +8,15 @@ def make_grid(inp: np.ndarray, nrow: int = 8, padding: int = 2, pad_value: float
 
     # convert to (N, C, H, W) with 3 channels
     if inp.ndim == 2:  # single image H x W
-        inp = np.expand_dims(x, axis=0)
+        inp = np.expand_dims(inp, axis=0)
     if inp.ndim == 3:  # single image C x H x W
         if inp.shape[0] == 1:  # if single-channel, convert to 3-channel
-            x = np.concatenate((x, x, x), axis=0)
-        x == np.expand_dims(x, axis=0)
+            inp = np.concatenate((inp, inp, inp), axis=0)
+        inp = np.expand_dims(inp, axis=0)
     if inp.ndim == 4 and inp.shape[1] == 1:  # single-channel images
-        x = np.concatenate((x, x, x), axis=1)
+        inp = np.concatenate((inp, inp, inp), axis=1)
+
+    assert inp.ndim == 4
 
     # make the mini-batch of images into a grid
     N, C, H, W = inp.shape
