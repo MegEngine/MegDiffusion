fe09d42d61259fb6af8a5362e843b22e8ccd6bf4
MegChai <acdoge.cao@gmail.com>
2022-07-07 18:55:08 +0800

    🔧 config: add config file for imagefolder (including celeba) dataset

--- megdiffusion/data/__init__.py
+++ megdiffusion/data/__init__.py
@@ -1,10 +1,8 @@
 from .cifar10 import build_cifar10_dataloader
-
-__all__ = [
-    "build_dataloader",
-]
+from .image_folder import build_imagefolder_dataloader
 
 def build_dataloader(dataset, dataset_dir, batch_size):
-    assert dataset in ["cifar10"]
     if dataset == "cifar10":
         return build_cifar10_dataloader(dataset_dir, batch_size)
+    else:
+        return build_imagefolder_dataloader(dataset_dir, batch_size)
---
+++ megdiffusion/data/image_folder.py
@@ -0,0 +1,16 @@
+import megengine.data as data
+import megengine.data.transform as T
+from megengine.data import Infinite
+from megengine.data.dataset import ImageFolder
+
+def build_imagefolder_dataloader(dataset_dir, batch_size):
+
+    transform = T.Compose([
+        T.RandomHorizontalFlip(),
+        T.ToMode(),
+    ])
+
+    train_dataset = ImageFolder(root=dataset_dir)
+    train_sampler = Infinite(data.RandomSampler(train_dataset, batch_size))
+    train_dataloader = data.DataLoader(train_dataset, train_sampler, transform)
+    return train_dataloader
