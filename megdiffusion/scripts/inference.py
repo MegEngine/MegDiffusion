45177defc4362eea80d6a4f78102b5954be54598
MegChai <acdoge.cao@gmail.com>
2022-07-10 08:19:05 +0800

    ✨ feat: support infer with model trained from scratch

--- megdiffusion/scripts/inference.py
+++ megdiffusion/scripts/inference.py
@@ -1,23 +1,42 @@
 import os
 from absl import app, flags
 
+import megengine as mge
 import megengine.functional as F
 
 from ..model import ddpm_cifar10
+from ..model.ddpm import UNet
 from ..diffusion import GaussionDiffusion
 from ..utils.transform import linear_scale_rev
 from ..utils.vision import make_grid, save_image
 
 FLAGS = flags.FLAGS
+flags.DEFINE_boolean("pretrain", True, help="use pre-trained model")
+flags.DEFINE_string("logdir", "./logs/DDPM_CIFAR10_EPS", help="log directory")
 flags.DEFINE_integer("timesteps", 1000, help="total diffusion steps")
 flags.DEFINE_integer("img_channels", 3, help="num of channels of training example")
 flags.DEFINE_integer("img_resolution", 32, help="image size of training example")
 flags.DEFINE_integer("sample_size", 64, "sampling size of images")
 flags.DEFINE_string("outputdir", "./output", help="log directory")
+# model architecture
+flags.DEFINE_integer("base_channel", 128, help="base channel of UNet")
+flags.DEFINE_multi_integer("chanel_multiplier", [1, 2, 2, 2], help="channel multiplier")
+flags.DEFINE_multi_integer("attention_resolutions", [16], help="resolutions use attension block")
+flags.DEFINE_integer("num_res_blocks", 2, help="number of resblock in each downblock")
+flags.DEFINE_float("dropout", 0.1, help="dropout rate of resblock")
 
 def infer():
     # model setup
-    model = ddpm_cifar10(pretrained=True)
+    if FLAGS.pretrain:
+        model = ddpm_cifar10(pretrained=True)
+    else:  # use model trained from scratch
+        assert os.path.isdir(FLAGS.logdir)
+        checkpoint = mge.load(os.path.join(FLAGS.logdir, "checkpoints", "ckpt.pkl"))
+        model = UNet(FLAGS.timesteps, FLAGS.img_resolution, FLAGS.img_channels, FLAGS.img_channels,
+            FLAGS.base_channel, FLAGS.chanel_multiplier, FLAGS.attention_resolutions,
+            FLAGS.num_res_blocks, FLAGS.dropout)
+        model.load_state_dict(checkpoint["ema_model"])
+
     model.eval()
 
     # diffusion setup
